3f597ce77f0f6459e6248268c192ec84028b6e43
Ricardo Pinelli <ricardo.pinelli@buildbox.com.br>
2023-12-29 22:48:04 -0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-extra-semi': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    curly: 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    semi: 0, // Disallow semicollons
+    'jsx-quotes': [1, 'prefer-single'], // JSX Single Quotes
+  },
 };
---
+++ src/styles/SafeArea.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native'
+
+export const SafeArea = styled.SafeAreaView`
+	margin-top: 32px;
+	padding: 0 24px;
+
+	/* marginTop: 32,
+		paddingHorizontal: 24, */
+`
